786da29599deb16ad17654fbc8f6e780baa920c7
Fraser Jubb <fraser.jubb@gmail.com>
2025-11-21 13:31:08 +0000

    Fist commit

---
+++ app.js
@@ -0,0 +1,6 @@
+// const input = document.querySelector('.test');
+// input.addEventListener('input', () => {
+//   input.value = input.value.replace(/\D/g, ''); // removes any non-digit characters
+// });
+
+// console.log('Heoll');
