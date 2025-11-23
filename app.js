b9acab300277a1f31e12abff200bd17a4531fd28
Fraser Jubb <fraser.jubb@gmail.com>
2025-11-23 12:14:25 +0000

    Tip amount is updating and calculating fine, code needs refactored still

--- app.js
+++ app.js
@@ -1,6 +1,49 @@
-// const input = document.querySelector('.test');
-// input.addEventListener('input', () => {
-//   input.value = input.value.replace(/\D/g, ''); // removes any non-digit characters
-// });
+const bill = document.querySelector('#bill-input');
+const people = document.querySelector('#people-input');
 
-// console.log('Heoll');
+const selectedTip = Array.from(document.querySelectorAll('.calculator__tip-amount'));
+
+const tipAmount = document.querySelector('#calculator__tip-amount');
+const totalString = document.querySelector('#total');
+
+const tipValue = Number(tipAmount.innerText.slice(1));
+const total = Number(totalString.innerText.slice(1));
+
+let peopleNum;
+function getPeople() {
+  if (!people.value) {
+    peopleNum = 0;
+  } else {
+    peopleNum = people.value;
+  }
+}
+
+let tipCalc;
+function updateTipAmount() {
+  getPeople();
+  if ((peopleNum === 0) | !bill.value | (myTip === 0)) {
+    tipCalc = 0;
+    // tipAmount.textContent = `$${tipCalc.toFixed(2)}`;
+  } else {
+    tipCalc = (bill.value * myTip) / peopleNum;
+  }
+  tipAmount.textContent = `$${tipCalc.toFixed(2)}`;
+}
+
+// const selected = document.querySelector('input[name="tip"]:checked');
+// console.log(selected);
+// if (selected) {
+//   console.log(selected.value);
+// }
+
+let myTip = 0;
+selectedTip.forEach(tip => {
+  tip.addEventListener('change', () => {
+    myTip = tip.value;
+    updateTipAmount();
+  });
+});
+
+people.addEventListener('change', updateTipAmount);
+bill.addEventListener('change', updateTipAmount);
+// updateTipAmount();
