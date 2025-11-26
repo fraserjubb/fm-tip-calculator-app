0a4a85dddd071d8156cd9d296093da0e242a6bd7
Fraser Jubb <fraser.jubb@gmail.com>
2025-11-26 18:55:25 +0000

    Variable and functions names improved, code shortened

--- app.js
+++ app.js
@@ -1,67 +1,45 @@
-const bill = document.querySelector('#bill-input');
-const people = document.querySelector('#people-input');
+const billInput = document.querySelector('#bill-input');
+const peopleInput = document.querySelector('#people-input');
 
 const selectedTip = Array.from(document.querySelectorAll('.calculator__tip-amount'));
-const tipButtons = Array.from(document.querySelectorAll('.calculator__tip-option'));
+const tipLabels = Array.from(document.querySelectorAll('.calculator__tip-option'));
 
-const tipAmount = document.querySelector('#calculator__tip-amount');
+const tipDisplay = document.querySelector('#calculator__tip-amount');
 const totalString = document.querySelector('#total');
 
-const tipValue = Number(tipAmount.innerText.slice(1));
 const total = Number(totalString.innerText.slice(1));
 
-let peopleNum;
-function getPeople() {
-  if (!people.value) {
-    peopleNum = 0;
-  } else {
-    peopleNum = people.value;
-  }
+let people;
+let bill;
+let tip;
+let tipPercent = 0;
+
+function getInputs() {
+  people = Number(peopleInput.value);
+  bill = Number(billInput.value);
 }
 
-let tipCalc;
 function updateTipAmount() {
-  getPeople();
-  if ((peopleNum === 0) | !bill.value | (myTip === 0)) {
-    tipCalc = 0;
-    // tipAmount.textContent = `$${tipCalc.toFixed(2)}`;
+  getInputs();
+  if (people === 0 || bill === 0 || tipPercent === 0) {
+    tip = 0;
   } else {
-    tipCalc = (bill.value * myTip) / peopleNum;
+    tip = (bill * tipPercent) / people;
   }
-  tipAmount.textContent = `$${tipCalc.toFixed(2)}`;
+  tipDisplay.textContent = `$${tip.toFixed(2)}`;
 }
 
-// const selected = document.querySelector('input[name="tip"]:checked');
-// console.log(selected);
-// if (selected) {
-//   console.log(selected.value);
-// }
-
-let myTip = 0;
-let tipSelected = false;
-
-function handleSelectedBtn(e) {
-  const button = e.nextElementSibling;
-  console.log(button.classList);
-  console.log(e.checked);
-  button.classList.toggle('calculator__tip-option--selected', e.checked);
-  // if (e.checked) {
-  // }
-  // button.classList.remove('calculator__tip-option--selected');
-}
-
-function handTips(e) {
+function getTip(e) {
   selectedTip.forEach(tip => {
-    const button = tip.nextElementSibling;
-    button.classList.toggle('calculator__tip-option--selected', tip.checked);
+    const tipValue = tip.nextElementSibling;
+    tipValue.classList.toggle('calculator__tip-option--selected', tip.checked);
   });
-  myTip = e.currentTarget.value;
+  tipPercent = e.currentTarget.value;
 
   updateTipAmount();
 }
 
-selectedTip.forEach(tip => tip.addEventListener('change', e => handTips(e)));
+selectedTip.forEach(tip => tip.addEventListener('change', e => getTip(e)));
 
-people.addEventListener('change', updateTipAmount);
-bill.addEventListener('change', updateTipAmount);
-// updateTipAmount();
+peopleInput.addEventListener('change', updateTipAmount);
+billInput.addEventListener('change', updateTipAmount);
